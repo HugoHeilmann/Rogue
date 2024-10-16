e4bae0c2554e7c2419dd2c5458a5e763f097f771
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-16 22:24:15 +0200

    #3 feat Redéfinition de la fonction __str__

--- Project/Maping/Coord.py
+++ Project/Maping/Coord.py
@@ -1,7 +1,10 @@
-class Coord():
+class Coord:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
 
     def __eq__(self, other) -> bool:
         return self.x == other.x and self.y == other.y
+
+    def __str__(self) -> str:
+        return f"<{self.x},{self.y}>"
--- tests/Maping/test_Coord.py
+++ tests/Maping/test_Coord.py
@@ -14,3 +14,10 @@ def test_equals():
     assert (c1 == c2) == True
     assert (c2 == c1) == True
     assert (c1 == c3) == False
+
+
+def test_str():
+    c1 = Coord(3, 4)
+    c2 = Coord(0, 0)
+    assert str(c1) == "<3,4>"
+    assert str(c2) == "<0,0>"
