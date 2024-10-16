a91b4647b401d16d96c75c4ae9b41eb33199bc68
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-16 22:58:42 +0200

    #6 feat Ajout d'un constructeur pour Map

--- Project/Maping/Map.py
+++ Project/Maping/Map.py
@@ -4,3 +4,12 @@ from Project.Maping.Coord import Coord
 class Map:
     ground = "."
     dir = {"z": Coord(-1, 0), "q": Coord(0, -1), "s": Coord(1, 0), "d": Coord(0, 1)}
+
+    def __init__(
+        self, size: int = 5, pos: Coord = Coord(1, 1), hero: str = "@"
+    ) -> None:
+        self.size = size
+        self.pos = pos
+        self.hero = hero
+        self._mat = [[Map.ground for _ in range(size)] for _ in range(size)]
+        self._elem = {hero: pos}
--- tests/Maping/test_Map.py
+++ tests/Maping/test_Map.py
@@ -9,3 +9,8 @@ def test_initialisation():
     assert map.dir["q"] == Coord(0, -1)
     assert map.dir["s"] == Coord(1, 0)
     assert map.dir["d"] == Coord(0, 1)
+
+    assert len(map._mat) == 5
+    for i in range(len(map._mat)):
+        assert len(map._mat[i]) == 5
+    assert map._elem["@"] == Coord(1, 1)
