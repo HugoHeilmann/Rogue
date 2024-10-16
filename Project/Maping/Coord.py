d5ae8ab122bb346ac1420c97ea1f7c4bcca300cb
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-16 22:15:50 +0200

    #2 feat Redéfinition de l'opérateur '=='

--- Project/Maping/Coord.py
+++ Project/Maping/Coord.py
@@ -2,3 +2,6 @@ class Coord():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
--- tests/Maping/test_Coord.py
+++ tests/Maping/test_Coord.py
@@ -1,6 +1,16 @@
 from Project.Maping.Coord import Coord
 
+
 def test_initialisation():
     coord = Coord(3, 0)
     assert coord.x == 3
     assert coord.y == 0
+
+
+def test_equals():
+    c1 = Coord(3, 4)
+    c2 = Coord(3, 4)
+    c3 = Coord(4, 4)
+    assert (c1 == c2) == True
+    assert (c2 == c1) == True
+    assert (c1 == c3) == False
