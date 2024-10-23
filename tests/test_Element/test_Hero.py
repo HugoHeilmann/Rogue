59575b32d43686fb8abb0883d6f3e2ce2045dc50
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-23 21:37:07 +0200

    #16 test Test de la classe Hero

---
+++ tests/test_Element/test_Hero.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+from Project.Element.Element import Element
+from Project.Element.Hero import Hero
+
+
+def test_initialisation():
+    h = Hero()
+    assert h._name == "Hero"
+    assert h._abbrv == "@"
+    assert h._hp == 10
+    assert h._strength == 2
+    assert h._inventory == []
+
+
+def test_take():
+    h = Hero()
+    s = Element("sword")
+    h.take(s)
+    assert h._inventory == [s]
+    assert h.description() == "<Hero>(10)[s]"
+
+
+def test_description():
+    h = Hero()
+    assert h.description() == "<Hero>(10)[]"
