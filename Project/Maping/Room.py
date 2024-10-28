29d8430d91779ff195a1e83551f39cb0f1895c44
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-28 14:39:55 +0100

    #25 feat Ajout des methodes center et intersect de la classe Room

--- Project/Maping/Room.py
+++ Project/Maping/Room.py
@@ -16,3 +16,14 @@ class Room:
             and c.y >= self._c1.y
             and c.y <= self._c2.y
         )
+
+    def center(self) -> Coord:
+        res: Coord = Coord(0, 0)
+        res.x = (self._c1.x + self._c2.x) // 2
+        res.y = (self._c1.y + self._c2.y) // 2
+        return res
+
+    def intersect(self, r: "Room") -> bool:
+        c3 = Coord(self._c2.x, self._c1.y)
+        c4 = Coord(self._c1.x, self._c2.y)
+        return self._c1 in r or self._c2 in r or c3 in r or c4 in r
