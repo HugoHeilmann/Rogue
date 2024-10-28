6b6261db9170490baaf0b75e0c413d187c92ff94
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-28 14:40:19 +0100

    #25 test Test des methodes center et intersect de la classe Room

--- tests/test_Maping/test_Room.py
+++ tests/test_Maping/test_Room.py
@@ -24,3 +24,16 @@ def test_contains():
     assert (Coord(4, 5) in r1) == True
     assert (Coord(4, 6) in r1) == False
     assert (Coord(5, 6) in r1) == False
+
+
+def test_center():
+    assert Room(Coord(0, 0), Coord(4, 5)).center() == Coord(2, 2)
+    assert Room(Coord(2, 7), Coord(8, 9)).center() == Coord(5, 8)
+    assert Room(Coord(2, 5), Coord(7, 8)).center() == Coord(4, 6)
+
+
+def test_intersect():
+    r = Room(Coord(5, 4), Coord(8, 9))
+    assert r.intersect(Room(Coord(0, 1), Coord(3, 2))) == False
+    assert r.intersect(Room(Coord(1, 2), Coord(10, 10))) == True
+    assert r.intersect(Room(Coord(8, 2), Coord(9, 12))) == True
