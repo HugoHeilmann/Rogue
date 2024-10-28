1d542086e1fff7fff0b47825e76288a720c9e730
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-10-28 15:11:23 +0100

    #26 test Adaptation des test de Map et test de addRoom

--- tests/test_Maping/test_Map.py
+++ tests/test_Maping/test_Map.py
@@ -5,24 +5,33 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"
 from Project.Element.Elements import Element, Hero
 from Project.Maping.Coord import Coord
 from Project.Maping.Map import Map
+from Project.Maping.Room import Room
 
 
 def test_initialisation():
-    map = Map()
+    map = Map(5)
     assert map.ground == "."
+    assert map.empty == " "
     assert map.dir["z"] == Coord(0, -1)
     assert map.dir["q"] == Coord(-1, 0)
     assert map.dir["s"] == Coord(0, 1)
     assert map.dir["d"] == Coord(1, 0)
+    assert map._elem == {}
+    assert map._rooms == []
+    assert map._roomsToReach == []
     assert len(map._mat) == 5
     for i in range(len(map._mat)):
         assert len(map._mat[i]) == 5
-    assert map._elem[map._hero] == Coord(1, 1)
+
+    for i in range(5):
+        for j in range(5):
+            assert map._mat[j][i] == Map.empty
+    # assert map._elem[map._hero] == Coord(1, 1)
 
 
 def test_len():
     map = Map()
-    assert len(map) == 5
+    assert len(map) == 20
     map = Map(3)
     assert len(map) == 3
 
@@ -30,52 +39,72 @@ def test_len():
 def test_contains():
     assert (Coord(0, 4) in Map()) == True
     assert (Coord(0, 4) in Map(3)) == False
-    assert (Coord(0, 5) in Map()) == False
+    assert (Coord(0, 5) in Map()) == True
     assert (Coord(-1, 3) in Map()) == False
     assert (Coord(1, -1) in Map()) == False
-    assert (Coord(5, 2) in Map()) == False
+    assert (Coord(5, 2) in Map()) == True
     assert (Coord(4, 4) in Map()) == True
 
 
 def test_str():
     map = Map(3, pos=Coord(0, 1))
-    assert str(map) == "...\n@..\n...\n"
+    map.put(map._pos, Hero())
+    assert str(map) == "   \n@  \n   \n"
 
 
 def test_get():
-    assert Map().get(Coord(0, 4)) == "."
-    assert str(Map().get(Coord(1, 1))) == "@"
-    assert Map().get(Coord(2, 3)) == "."
+    map = Map(5)
+    map.put(Coord(1, 1), Hero())
+    assert map.get(Coord(0, 4)) == " "
+    assert str(map.get(Coord(1, 1))) == "@"
+    assert map.get(Coord(2, 3)) == " "
 
 
 def test_pos():
-    map = Map(pos=Coord(1, 1))
-    assert map.get_pos(map._hero) == Coord(1, 1)
+    map = Map(5, pos=Coord(1, 1))
+    h = Hero()
+    map.put(map._pos, h)
+    assert map.get_pos(h) == Coord(1, 1)
     h2 = Hero(_abbrv="X")
-    assert Map(pos=Coord(2, 3), hero=h2).get_pos(h2) == Coord(2, 3)
+    map = Map(pos=Coord(2, 3))
+    map.put(map._pos, h2)
+    assert map.get_pos(h2) == Coord(2, 3)
 
 
 def test_put():
-    m = Map()
+    m = Map(5)
     m.put(Coord(3, 2), "X")
     m.put(Coord(0, 0), "A")
-    assert str(m) == "A....\n.@...\n...X.\n.....\n.....\n"
-    assert m._elem == {m._hero: Coord(1, 1), "X": Coord(3, 2), "A": Coord(0, 0)}
+    assert str(m) == "A    \n     \n   X \n     \n     \n"
+    assert m._elem == {"X": Coord(3, 2), "A": Coord(0, 0)}
+
+
+def test_addRoom():
+    m = Map(6)
+    m.addRome(Room(Coord(1, 1), Coord(4, 3)))
+    assert str(m) == "      \n .... \n .... \n .... \n      \n      \n"
+    assert m._rooms == []
+    assert len(m._roomsToReach) == 1
+    assert str(m._roomsToReach[0]) == "[<1,1>,<4,3>]"
 
 
 def test_way():
-    m = Map(3, pos=Coord(0, 1), hero=Hero())
+    h = Hero()
+    m = Map(3, pos=Coord(0, 1), hero=h)
     m.put(Coord(0, 2), Element("Sword"))
-    assert str(m) == "...\n@..\nS..\n"
+    m.put(m._pos, h)
+    assert str(m) == "   \n@  \nS  \n"
     assert m._hero.description() == "<Hero>(10)[]"
     m.move(m._hero, Coord(0, 1))
-    assert str(m) == "...\n@..\n...\n"
+    assert str(m) == "   \n@  \n.  \n"
     assert m._hero.description() == "<Hero>(10)[S]"
     assert m._elem == {m._hero: Coord(0, 1)}
 
 
 def test_map():
-    m = Map(3, hero=Hero())
-    assert str(m) == "...\n.@.\n...\n"
+    h = Hero()
+    m = Map(3, hero=h)
+    m.put(m._pos, h)
+    assert str(m) == "   \n @ \n   \n"
     assert m._hero.description() == "<Hero>(10)[]"
     assert m._elem[m._hero] == Coord(1, 1)
