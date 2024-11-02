10038ead913583ded392f28de4718e6d518989a7
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-03 00:58:55 +0100

    #52 feat methode moveAllMonsters

--- Project/Element/Elements.py
+++ Project/Element/Elements.py
@@ -26,68 +26,124 @@ class Element(metaclass=abc.ABCMeta):
 
 
 class Equipment(Element):
-    def __init__(self, _name: str, _abbrv: str = "") -> None:
-        Element.__init__(self, _name, _abbrv)
+    def __init__(self, name: str, abbrv: str = "") -> None:
+        Element.__init__(self, name, abbrv)
 
     def meet(self, hero: "Hero") -> bool:
+        theGame().addMessage("You pick up a " + str(self._name))
         hero.take(self)
-        theGame().addMessage(f"You pick up a {str(self._name)}")
         return True
 
 
 class Creature(Element):
-    def __init__(self, _name: str, _hp: int, _abbrv: str = "", _strength: int = 1):
-        Element.__init__(self, _name, _abbrv)
-        self._hp = _hp
-        self._strength = _strength
+    def __init__(self, name: str, hp: int, abbrv: str = "", strength: int = 1) -> None:
+        Element.__init__(self, name, abbrv)
+        self._hp = hp
+        self._strength = strength
 
     def description(self) -> str:
-        return super().description() + "(" + str(self._hp) + ")"
+        return Element.description(self) + "(" + str(self._hp) + ")"
 
     def meet(self, other: "Creature") -> bool:
         self._hp -= other._strength
-        theGame().addMessage(f"The {other._name} hits the {self.description()}")
+        theGame().addMessage(
+            "The " + str(other._name) + " hits the " + str(self.description())
+        )
         return self._hp <= 0
 
 
 class Hero(Creature):
     def __init__(
-        self, _name: str = "Hero", _hp: int = 10, _abbrv: str = "@", _strength: int = 2
-    ):
-        Creature.__init__(self, _name, _hp, _abbrv, _strength)
+        self, name: str = "Hero", hp: int = 10, abbrv: str = "@", strength: int = 2
+    ) -> None:
+        Creature.__init__(self, name, hp, abbrv, strength)
         self._inventory = []
 
-    def take(self, elem: Equipment):
-        if not (isinstance(elem, Equipment)):
-            raise TypeError("Not an Equipment")
-        self._inventory.append(elem)
+    def description(self) -> str:
+        return Creature.description(self) + str(self._inventory)
 
-    def description(self):
-        return super().description() + str(self._inventory)
+    def take(self, elem: Equipment) -> None:
+        self._inventory.append(elem)
+        if isinstance(elem, Equipment) == False:
+            raise TypeError("Not in equipment")
+
+
+class Room:
+    def __init__(self, c1: Coord, c2: Coord) -> None:
+        self._c1 = c1
+        self._c2 = c2
+
+    def __repr__(self) -> str:
+        return "[" + str(self._c1) + ", " + str(self._c2) + "]"
+
+    def __contains__(self, other: Coord) -> bool:
+        if isinstance(other, Coord):
+            return (
+                min(self._c1.x, self._c2.x) <= other.x <= self._c1.x
+                and min(self._c1.y, self._c2.y) <= other.y <= self._c1.y
+                or min(self._c1.x, self._c2.x) <= other.x <= self._c2.x
+                and min(self._c1.y, self._c2.y) <= other.y <= self._c2.y
+            )
+
+    def center(self) -> Coord:
+        cx: int = (self._c1.x + self._c2.x) // 2
+        cy: int = (self._c1.y + self._c2.y) // 2
+        return Coord(cx, cy)
+
+    def intersect(self, other: "Room") -> bool:
+        c3s = Coord(self._c1.x, self._c2.y)
+        c4s = Coord(self._c2.x, self._c1.y)
+        c3o = Coord(other._c1.x, other._c2.y)
+        c4o = Coord(other._c2.x, other._c1.y)
+        return (
+            other._c1 in self
+            or other._c2 in self
+            or c3o in self
+            or c4o in self
+            or self._c1 in other
+            or self._c2 in other
+            or c3s in other
+            or c4s in other
+        )
+
+    def randCoord(self) -> Coord:
+        rdmX = random.randint(self._c1.x, self._c2.x)
+        rdmY = random.randint(self._c1.y, self._c2.y)
+        return Coord(rdmX, rdmY)
+
+    def randEmptyCoord(self, map: "Map") -> Coord:
+        while True:
+            res: Coord = self.randCoord()
+            if map.get(res) == map.ground and res != self.center():
+                return res
+
+    def decorate(self, map: "Map") -> None:
+        cea: Coord = self.randEmptyCoord(map)
+        ea: Equipment = theGame().randEquipment()
+        map.put(cea, ea)
+        cma: Coord = self.randEmptyCoord(map)
+        ma: Creature = theGame().randMonster()
+        map.put(cma, ma)
 
 
 class Map:
-    empty = " "
     ground = "."
-    dir = {"z": Coord(0, -1), "q": Coord(-1, 0), "s": Coord(0, 1), "d": Coord(1, 0)}
+    empty = " "
+    dir = {"z": Coord(0, -1), "s": Coord(0, 1), "d": Coord(1, 0), "q": Coord(-1, 0)}
 
-    def __init__(
-        self,
-        size: int = 20,
-        hero: Hero = Hero(),
-        nbrooms: int = 7,
-    ) -> None:
-        self._size = size
-        self._hero = hero
-        self._mat = [[Map.empty for _ in range(size)] for _ in range(size)]
-        self._elem: Dict[Element, Coord] = {}
-        self._roomsToReach: List[Room] = []
+    def __init__(self, size: int = 20, hero: Hero = Hero(), nbrooms: int = 7) -> None:
+        self._mat = []
         self._rooms: List[Room] = []
+        self._roomsToReach: List[Room] = []
+        self._elem: Dict[Element, Coord] = {}
+        self._hero = hero
+        for i in range(size):
+            self._mat.append([Map.empty] * size)
         self.generateRooms(nbrooms)
         self.reachAllRooms()
-        for room in self._rooms:
-            room.decorate(self)
         self.put(self._rooms[0].center(), self._hero)
+        for room in self._rooms:
+            Room.decorate(room, self)
 
     def __len__(self) -> int:
         return len(self._mat)
@@ -95,65 +151,85 @@ class Map:
     def __contains__(self, item) -> bool:
         if isinstance(item, Coord):
             return 0 <= item.x < len(self) and 0 <= item.y < len(self)
-        else:
-            return item in self._elem
+        return item in self._elem
 
     def __repr__(self) -> str:
-        matrix = ""
-        for i in range(len(self._mat)):
-            for j in self._mat[i]:
-                matrix += str(j)
-            matrix += "\n"
-        return matrix
+        s = ""
+        for i in self._mat:
+            for j in i:
+                s += str(j)
+            s += "\n"
+        return s
 
-    def get(self, c: Coord):
+    def put(self, c: Coord, o: Element) -> None:
+        self.checkCoord(c)
+        self.checkElement(o)
+        if self._mat[c.y][c.x] != Map.ground:
+            raise ValueError("Incorrect cell")
+        if o in self:
+            raise KeyError("Already placed")
+        self._mat[c.y][c.x] = o
+        self._elem[o] = c
+
+    def get(self, c: Coord) -> Element:
         self.checkCoord(c)
         return self._mat[c.y][c.x]
 
-    def get_pos(self, e: Element) -> Coord:
-        self.checkElement(e)
-        return self._elem[e]
-
-    def put(self, c: Coord, e: Element) -> None:
-        self.checkCoord(c)
-        self.checkElement(e)
-        if self._mat[c.y][c.x] != Map.ground:
-            raise ValueError("Incorect cell")
-        if e in self:
-            raise KeyError("Already placed")
-
-        self._mat[c.y][c.x] = e
-        self._elem[e] = c
+    def pos(self, o: Element) -> Coord:
+        self.checkElement(o)
+        return self._elem[o]
 
     def rm(self, c: Coord) -> None:
         self.checkCoord(c)
-        del self._elem[self.get(c)]
+        del self._elem[self._mat[c.y][c.x]]
         self._mat[c.y][c.x] = Map.ground
 
-    def addRoom(self, r: "Room"):
-        self._roomsToReach.append(r)
-        for i in range(r._c1.x, r._c2.x + 1):
-            for j in range(r._c1.y, r._c2.y + 1):
-                self._mat[j][i] = Map.ground
+    def move(self, e: Element, way: Coord) -> None:
+        orig = self.pos(e)
+        dest = orig + way
+        if dest in self:
+            if self.get(dest) == Map.ground:
+                self._mat[orig.y][orig.x] = Map.ground
+                self._mat[dest.y][dest.x] = e
+                self._elem[e] = dest
+            elif (
+                self.get(dest) != Map.empty
+                and self.get(dest).meet(e)
+                and self.get(dest) != self._hero
+            ):
+                self.rm(dest)
 
-    def findRoom(self, c: Coord) -> Union["Room", bool]:
-        for r in self._roomsToReach:
-            if c in r:
-                return r
+    def addRoom(self, room: Room) -> None:
+        i = 0
+        while i <= room._c2.y:
+            j = 0
+            if room._c1.y <= i <= room._c2.y:
+                while j <= room._c2.x:
+                    if room._c1.x <= j <= room._c2.x:
+                        self._mat[i][j] = Map.ground
+                    j += 1
+            i += 1
+        self._roomsToReach.append(room)
+
+    def findRoom(self, coord: Coord) -> Union[Room, bool]:
+        for room in self._roomsToReach:
+            if coord in room:
+                return room
         return False
 
-    def intersectNone(self, room: "Room") -> bool:
-        for r in self._roomsToReach:
-            if room.intersect(r):
+    def intersectNone(self, room: Room) -> bool:
+        for place in self._roomsToReach:
+            if place.intersect(room):
                 return False
         return True
 
-    def dig(self, c: Coord) -> None:
-        self._mat[c.y][c.x] = Map.ground
-        if self.findRoom(c) != False:
-            r: Room = self.findRoom(c)
-            self._roomsToReach.remove(r)
-            self._rooms.append(r)
+    def dig(self, coord: Coord) -> None:
+        inside = self.findRoom(coord)
+        if inside != False:
+            self._roomsToReach.remove(inside)
+            self._rooms.append(inside)
+        else:
+            self._mat[coord.y][coord.x] = Map.ground
 
     def corridor(self, start: Coord, end: Coord) -> None:
         if start.y > end.y:
@@ -175,66 +251,51 @@ class Map:
         self.dig(end)
 
     def reach(self) -> None:
-        roomA: Room = random.choice(self._rooms)
-        roomB: Room = random.choice(self._roomsToReach)
-        self.corridor(roomA.center(), roomB.center())
+        begin: Room = random.choice(self._rooms)
+        end: Room = random.choice(self._roomsToReach)
+        centA: Coord = Room.center(begin)
+        centB: Coord = Room.center(end)
+        self.corridor(centA, centB)
 
     def reachAllRooms(self) -> None:
-        self._rooms.append(self._roomsToReach.pop())
-        while self._roomsToReach != []:
+        self._rooms.append(self._roomsToReach.pop(0))
+        while len(self._roomsToReach) > 0:
             self.reach()
 
-    def randRoom(self) -> "Room":
-        x1: int = random.randint(0, len(self) - 3)
-        y1: int = random.randint(0, len(self) - 3)
-        height: int = random.randint(3, 9)
-        width: int = random.randint(3, 9)
-        x2: int = min(len(self) - 1, x1 + width)
-        y2: int = min(len(self) - 1, y1 + height)
+    def randRoom(self) -> Room:
+        x1 = random.randint(0, len(self) - 3)
+        y1 = random.randint(0, len(self) - 3)
+        largeur = random.randint(3, 8)
+        hauteur = random.randint(3, 8)
+        x2 = min(len(self) - 1, x1 + largeur)
+        y2 = min(len(self) - 1, y1 + hauteur)
         return Room(Coord(x1, y1), Coord(x2, y2))
 
     def generateRooms(self, n: int) -> None:
         for _ in range(n):
-            r: Room = self.randRoom()
-            if self.intersectNone(r):
-                self.addRoom(r)
+            newPlace = self.randRoom()
+            if self.intersectNone(newPlace):
+                self.addRoom(newPlace)
 
-    def move(self, e: Element, way: Coord) -> None:
-        c2 = self.get_pos(e) + way
-        if c2 not in self:
-            return
-        if self.get(c2) == Map.empty:
-            return
-        if c2 in self and self.get(c2) == Map.ground:
-            self.rm(self.get_pos(e))
-            self.put(c2, e)
-        else:
-            if c2 in self and self.get(c2).meet(e) == True:
-                self.rm(c2)
-
-    def checkCoord(self, c: Coord) -> None:
-        if not (isinstance(c, Coord)):
+    def checkCoord(self, c) -> Union[None, TypeError, IndexError]:
+        if isinstance(c, Coord) == False:
             raise TypeError("Not a Coord")
-        if c.x < 0 or c.y < 0 or c.x > len(self) or c.y > len(self):
-            raise IndexError("Out of map Coord")
+        if c not in self:
+            raise IndexError("Out of map coord")
 
-    def checkElement(self, e) -> None:
-        if not (isinstance(e, Element)):
-            raise TypeError("Not an Element")
+    def checkElement(self, o) -> Union[None, TypeError]:
+        if not isinstance(o, Element):
+            raise TypeError("Not a Element")
 
-
-class object:
-    def __init__(self, hero: Hero = None, level: int = 1, floor: Map = None) -> None:
-        if hero == None:
-            self._hero: Hero = Hero()
-        else:
-            self._hero: Hero = hero
-        self._level: int = level
-        self._floor: Map = floor
-        self._message: List[str] = []
+    def moveAllMonsters(self) -> None:
+        target: Coord = self.pos(self._hero)
+        for elem in self._elem:
+            badGuy: Coord = self.pos(elem)
+            if type(elem) == Creature and Coord.distance(badGuy, target) <= 6:
+                self.move(elem, Coord.direction(badGuy, target))
 
 
-class Game(object):
+class Game:
     equipments = {
         0: [Equipment("potion", "!"), Equipment("gold", "o")],
         1: [Equipment("sword"), Equipment("bow")],
@@ -242,134 +303,57 @@ class Game(object):
     }
     monsters = {
         0: [Creature("Goblin", 4), Creature("Bat", 2, "W")],
-        1: [Creature("Ork", 6, _strength=2), Creature("Blob", 10)],
-        5: [Creature("Dragon", 20, _strength=3)],
+        1: [Creature("Ork", 6, strength=2), Creature("Blob", 10)],
+        5: [Creature("Dragon", 20, strength=3)],
     }
 
-    def __init__(self, hero: Hero = None, level: int = 1, floor: Map = None) -> None:
-        object.__init__(self, hero, level, floor)
+    def __init__(self, hero: Hero = Hero(), level: int = 1):
+        self._hero = hero
+        self._level = level
+        self._floor = None
+        self._message: List[str] = []
 
     def buildFloor(self) -> Map:
         self._floor = Map(hero=self._hero)
-        self._floor.put(self._floor._rooms[-1].center(), Stairs())
+        theGame()._floor = self._floor
+        stairs = Room.center(theGame()._floor._rooms[-1])
+        theGame()._floor.put(stairs, Stairs())
         return self._floor
 
     def addMessage(self, msg: str) -> None:
         self._message.append(msg)
 
     def readMessages(self) -> str:
-        res: str = ""
-        for msg in self._message:
-            res += msg
-            res += ". "
-        self._message = []
-        return res
+        msg: str = ""
+        for message in self._message:
+            msg += message
+            msg += ". "
+        self._message.clear()
+        return msg
 
-    def randElement(
-        self, collection: Dict[int, List[Union[Equipment, Creature]]]
-    ) -> Union[Equipment, Creature]:
-        x: int = math.floor(random.expovariate(1 / self._level))
-        while x not in self.monsters.keys():
-            x -= 1
-        for rarity in collection:
-            if rarity == x:
-                elements = collection[rarity]
-        return copy.copy(random.choice(elements))
+    def randElement(self, collection) -> Element:
+        X = random.expovariate(1 / self._level)
+        for i in collection:
+            if i < X:
+                l = collection[i]
+        rdm = random.choice(l)
+        return copy.copy(rdm)
 
     def randEquipment(self) -> Equipment:
-        return self.randElement(self.equipments)
+        return self.randElement(Game.equipments)
 
     def randMonster(self) -> Creature:
-        return self.randElement(self.monsters)
-
-    def play(self):
-        print("--- Welcome Hero! ---")
-        while self._hero._hp > 0:
-            print()
-            print(self._floor)
-            print(self._hero.description())
-            print(self.readMessages())
-            self._floor.move(self._hero, Map.dir[getch()])
-        print("--- Game Over ---")
-
-
-class Room:
-    def __init__(self, c1: Coord, c2: Coord) -> None:
-        self._c1 = c1
-        self._c2 = c2
-
-    def __repr__(self) -> str:
-        return f"[<{self._c1.x},{self._c1.y}>,<{self._c2.x},{self._c2.y}>]"
-
-    def __contains__(self, c: Coord) -> bool:
-        return (
-            c.x >= self._c1.x
-            and c.x <= self._c2.x
-            and c.y >= self._c1.y
-            and c.y <= self._c2.y
-        )
-
-    def center(self) -> Coord:
-        res: Coord = Coord(0, 0)
-        res.x = (self._c1.x + self._c2.x) // 2
-        res.y = (self._c1.y + self._c2.y) // 2
-        return res
-
-    def intersect(self, r: "Room") -> bool:
-        c3: Coord = Coord(self._c2.x, self._c1.y)
-        c4: Coord = Coord(self._c1.x, self._c2.y)
-        return self._c1 in r or self._c2 in r or c3 in r or c4 in r
-
-    def randCoord(self) -> Coord:
-        x: int = random.randint(self._c1.x, self._c2.x)
-        y: int = random.randint(self._c1.y, self._c2.y)
-        return Coord(x, y)
-
-    def randEmptyCoord(self, map: Map) -> Coord:
-        while True:
-            res: Coord = self.randCoord()
-            if map.get(res) == map.ground and res != self.center():
-                return res
-
-    def decorate(self, map: Map) -> None:
-        randCoordEquip: Coord = self.randEmptyCoord(map)
-        randEquip: Equipment = theGame().randEquipment()
-        map.put(randCoordEquip, randEquip)
-
-        randCoordMonster: Coord = self.randEmptyCoord(map)
-        randMonster: Creature = theGame().randMonster()
-        map.put(randCoordMonster, randMonster)
+        return self.randElement(Game.monsters)
 
 
 class Stairs(Element):
-    def __init__(self, name="Stairs", abbrv="E"):
+    def __init__(self, name="Stairs", abbrv="E") -> None:
         Element.__init__(self, name, abbrv)
 
-    def meet(self, hero: "Hero") -> bool:
-        theGame()._level += 1
-        theGame().buildFloor()
+    def meet(self, hero: Hero) -> bool:
         theGame().addMessage(f"{hero._name} goes down")
+        return True
 
 
-def theGame(game=Game()) -> Game:
+def theGame(game=Game()):
     return game
-
-
-def getch():
-    try:
-        import sys
-        import termios
-        import tty
-
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-    except ImportError:
-        import msvcrt
-
-        return msvcrt.getch().decode("utf-8")
