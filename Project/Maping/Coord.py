6c4b98596fcffc2c009a82bc031e8fa402cd1e2d
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-02 23:40:40 +0100

    #50 feat Ajout d'une methode de direction

--- Project/Maping/Coord.py
+++ Project/Maping/Coord.py
@@ -22,3 +22,14 @@ class Coord:
         sqrx: int = math.pow((other.x - self.x), 2)
         sqry: int = math.pow((other.y - self.y), 2)
         return math.sqrt(sqrx + sqry)
+
+    def direction(self, other: "Coord") -> "Coord":
+        diff: int = self - other
+        cos = diff.x / self.distance(other)
+        if cos > 1 / math.sqrt(2):
+            return Coord(-1, 0)
+        if cos < -1 / math.sqrt(2):
+            return Coord(1, 0)
+        if diff.y > 0:
+            return Coord(0, -1)
+        return Coord(0, 1)
