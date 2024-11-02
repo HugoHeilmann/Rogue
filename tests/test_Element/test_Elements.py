afb1e884e9a7fe5edc0f9589a0e4130f353ddb1d
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-11-02 22:44:34 +0100

    #46 test Test des fonctions random

--- tests/test_Element/test_Elements.py
+++ tests/test_Element/test_Elements.py
@@ -235,17 +235,6 @@ def test_move():
     assert m._elem == {h: Coord(1, 2)}
 
 
-def test_move_not_posssible():
-    m = Map(5)
-    assert m._elem == {m._hero: Coord(1, 2)}
-    m.move(m._hero, Coord(0, -1))
-    assert m._elem == {m._hero: Coord(1, 1)}
-    m.move(m._hero, Coord(0, -1))
-    assert m._elem == {m._hero: Coord(1, 0)}
-    m.move(m._hero, Coord(0, -1))
-    assert m._elem == {m._hero: Coord(1, 0)}
-
-
 def test_map():
     h = Hero()
     m = Map(3, hero=h)
@@ -323,3 +312,66 @@ def test_check():
     except TypeError:
         print("Test passed")
         assert True == True
+
+
+# Room
+
+
+def test_str():
+    r1 = Room(Coord(2, 3), Coord(4, 5))
+    assert str(r1._c1) == "<2,3>"
+    assert str(r1._c2) == "<4,5>"
+    assert str(r1) == "[<2,3>,<4,5>]"
+
+
+def test_contains():
+    r1 = Room(Coord(2, 3), Coord(4, 5))
+    assert (Coord(0, 1) in r1) == False
+    assert (Coord(2, 1) in r1) == False
+    assert (Coord(2, 3) in r1) == True
+    assert (Coord(4, 4) in r1) == True
+    assert (Coord(3, 4) in r1) == True
+    assert (Coord(4, 5) in r1) == True
+    assert (Coord(4, 6) in r1) == False
+    assert (Coord(5, 6) in r1) == False
+
+
+def test_center():
+    assert Room(Coord(0, 0), Coord(4, 5)).center() == Coord(2, 2)
+    assert Room(Coord(2, 7), Coord(8, 9)).center() == Coord(5, 8)
+    assert Room(Coord(2, 5), Coord(7, 8)).center() == Coord(4, 6)
+
+
+def test_intersect():
+    r = Room(Coord(5, 4), Coord(8, 9))
+    assert r.intersect(Room(Coord(0, 1), Coord(3, 2))) == False
+    assert r.intersect(Room(Coord(1, 2), Coord(10, 10))) == True
+    assert r.intersect(Room(Coord(8, 2), Coord(9, 12))) == True
+
+
+def test_randCoord():
+    random.seed(22)
+    assert Room(Coord(0, 3), Coord(7, 6)).randCoord() == Coord(2, 4)
+    assert Room(Coord(10, 13), Coord(47, 56)).randCoord() == Coord(11, 52)
+
+
+def test_randEmptyCoord():
+    m = Map(3)
+    m.rm(Coord(1, 1))
+    m.put(Coord(2, 1), Equipment("x"))
+    m.put(Coord(0, 2), Equipment("x"))
+    random.seed(0)
+    assert m._rooms[0].randEmptyCoord(m) == Coord(0, 1)
+    assert m._rooms[0].randEmptyCoord(m) == Coord(2, 0)
+    assert m._rooms[0].randEmptyCoord(m) == Coord(2, 0)
+    assert m._rooms[0].randEmptyCoord(m) == Coord(1, 0)
+
+
+def test_decorate():
+    random.seed(1)
+    m = Map(20)
+    print(m)
+    m._rooms[0].decorate(m)
+    m._rooms[1].decorate(m)
+    print(m)
+    print(m._elem)
